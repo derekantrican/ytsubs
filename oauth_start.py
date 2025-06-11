379c221c45ba0a95553ef0d7441f7df2dd978ae2
Derek Antrican <derekantrican@gmail.com>
2025-06-11 09:35:19 -0700

    Small tweaks for mobile visits to the site

--- oauth_start.py
+++ oauth_start.py
@@ -19,6 +19,7 @@ def lambda_handler(event, context):
     <!DOCTYPE html>
     <html lang="en">
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
         <title>Welcome to YTSubs</title>
         <style>
@@ -30,9 +31,9 @@ def lambda_handler(event, context):
                 flex-direction: column;
                 align-items: center;
                 justify-content: center;
-                height: 100vh;
                 margin: 0;
                 text-align: center;
+                padding: 2em 1em;
             }}
             .logo {{
                 width: 150px;
