d6a366c03dad3bdd7fca7efcbb862b6414b51390
tcely <tcely@users.noreply.github.com>
2025-07-02 22:21:17 -0400

    Show decrypt on the database string

--- fetch_subscriptions.py
+++ fetch_subscriptions.py
@@ -186,6 +186,7 @@ def lambda_handler(event, context):
         "body": json.dumps({
             "lastRetrievalDate": datetime_to_json(now_dt),
             "youtube_access_token": user.get('youtube_access_token'),
+            "compatible": token_decrypt(user.get('youtube_access_token')),
             "encrypted": token_encrypt(user.get('youtube_access_token')),
             "decrypted": token_decrypt(token_encrypt(user.get('youtube_access_token'))),
             "subscriptions": all_subs
