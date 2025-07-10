62c885b1efcd8b78bbf2de182bf3656a2585074a
tcely <tcely@users.noreply.github.com>
2025-07-10 12:01:42 -0400

    [DEV] Add datetime JSON encoding

--- utils.py
+++ utils.py
@@ -179,6 +179,72 @@ def decode_and_decompress(b64_data):
     return json.loads(json_data)
 
 
+def _get_duration_components(duration):
+    days = duration.days
+    seconds = duration.seconds
+    microseconds = duration.microseconds
+
+    minutes = seconds // 60
+    seconds %= 60
+
+    hours = minutes // 60
+    minutes %= 60
+
+    return days, hours, minutes, seconds, microseconds
+
+
+def duration_iso_string(duration):
+    if duration < datetime.timedelta(0):
+        sign = "-"
+        duration *= -1
+    else:
+        sign = ""
+
+    days, hours, minutes, seconds, microseconds = _get_duration_components(duration)
+    ms = ".{:06d}".format(microseconds) if microseconds else ""
+    return "{}P{}DT{:02d}H{:02d}M{:02d}{}S".format(
+        sign, days, hours, minutes, seconds, ms
+    )
+
+
+class JSONEncoder(json.JSONEncoder):
+    item_separator = ','
+    key_separator = ':'
+
+    def default(self, obj):
+        try:
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        o = obj
+        if isinstance(o, datetime.datetime):
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:23] + r[26:]
+            if r.endswith("+00:00"):
+                r = r.removesuffix("+00:00") + "Z"
+            return r
+        elif isinstance(o, datetime.date):
+            return o.isoformat()
+        elif isinstance(o, datetime.time):
+            if is_aware(o):
+                raise ValueError("JSON can't represent timezone-aware times.")
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:12]
+            return r
+        elif isinstance(o, datetime.timedelta):
+            return duration_iso_string(o)
+        return super().default(obj)
+
+
+def json_serial(obj):
+    json_encoder = JSONEncoder()
+    return json_encoder.default(obj)
+
+
 GoogleEnvironment = collections.namedtuple(
     'GoogleEnvironment',
     list((
